27cd110c280bbab745eaefcf4c3b6a4ed9aa5dea
Samuel de Moura <samuel.moura@lavid.ufpb.br>
2020-02-14 17:12:04 -0300

    Implement CLI for executing pipelines

---
+++ cli.py
@@ -0,0 +1,24 @@
+import sys
+
+import pipeline
+
+# Application entry point.
+if __name__ == '__main__':
+    '''
+    if len(sys.argv) < 2:
+        print(f'Usage: python3 {sys.argv[0]} <pipeline expression>')
+        quit()
+    '''
+
+    pipeline_expression = ' '.join(sys.argv[1:])
+    if not pipeline_expression:
+        # User didn't give a pipeline expression. Use default.
+        pipeline_expression = 'translate src=./data/corpus.csv'
+        print(
+            f'[WARN] No pipeline specified. Defaulting to: "{pipeline_expression}"'
+        )
+
+    pl = pipeline.Pipeline()
+    pl.parse_pipeline_expression(pipeline_expression)
+    pl.instantiate_elements()
+    pl.process()
---
+++ pipeline.py
@@ -0,0 +1,88 @@
+import logging
+
+import registry
+from elements import *
+
+logger = logging.getLogger(__name__)
+
+# Log to file.
+log_file_handler = logging.FileHandler('pipeline.log')
+log_file_handler.setLevel(logging.INFO)
+log_file_handler.setFormatter(logging.Formatter('[%(levelname)s] [%(asctime)s] %(message)s'))
+logger.addHandler(log_file_handler)
+
+# Log to stdout.
+logging.basicConfig(
+    format='[%(levelname)s] [%(asctime)s] %(message)s',
+    level=logging.DEBUG,
+)
+
+class Pipeline():
+    '''Called by the CLI interface.'''
+
+    def __init__(self):
+        # Holds the pipeline elements.
+        self._pipeline = []
+
+    def parse_pipeline_expression(self, pipeline_expression):
+        '''Takes a user-given `pipeline_expression`, parses it and populates
+        `self._pipeline` with dictionaries containing the fields `name`,
+        `class`, `parameters` and a null `instance`.
+        '''
+        pipeline_elements = pipeline_expression.split(' ! ')
+
+        for element_expression in pipeline_elements:
+            logger.debug(f'Processing element expression: "{element_expression}"')
+
+            first_space = element_expression.find(' ')
+
+            # User-friendly element name. Used to fetch from registry.
+            element_name = element_expression[:first_space]
+
+            # User-friendly element parameters list. For example:
+            # ['src=Input File.txt', 'enable_masking']
+            element_parameters_as_str = element_expression[first_space + 1:].split(',')
+
+            # Dictionary version of the parameter list described above.
+            element_parameters_dict = {}
+
+            # Parse parameters string into parameter dictionary.
+            for element_parameter_as_str in element_parameters_as_str:
+                key_val_tuple = element_parameter_as_str.split('=')
+                if len(key_val_tuple) == 2:
+                    # Is a key-value tuple, assign both.
+                    element_parameters_dict[key_val_tuple[0]] = key_val_tuple[1]
+                elif len(key_val_tuple) == 1:
+                    # Is a key-only tuple, assign True to key.
+                    element_parameters_dict[key_val_tuple[0]] = True
+
+            # Add element class and parameters to the pipeline.
+            element = {
+                'name': element_name,
+                'class': registry.get_element(element_name),
+                'parameters': element_parameters_dict,
+                'instance': None,
+            }
+
+            self._pipeline.append(element)
+
+    def instantiate_elements(self):
+        '''Calls the constructor with the desired parameters for each element
+        in the pipeline.
+        '''
+        for element in self._pipeline:
+            try:
+                logger.debug('Instantiating "{}"...'.format(element['name']))
+                element['instance'] = element['class'](**element['parameters'])
+            except Exception as ex:
+                logger.error('Failed to instantiate "{}": {}'.format(element['name'], ex))
+                raise ex
+
+    def process(self):
+        '''Starts the pipeline processing.'''
+        for element in self._pipeline:
+            instance = element['instance']
+
+            # TODO: implement this. careful here, need to handle both generators
+            # and regular functions
+            pass

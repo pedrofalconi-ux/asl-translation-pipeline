be8999d0ad1c77acb72c23fc748230267a3cda0f
Samuel de Moura <samuel.moura@lavid.ufpb.br>
2020-02-14 17:11:21 -0300

    Implement registry for pipeline elements

---
+++ elements/__init__.py
@@ -0,0 +1,9 @@
+import glob
+from os.path import dirname, basename, isfile, join
+
+# Put all element modules into the __all__ variable (used when importing *)
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [
+    basename(f)[:-3] for f in modules
+    if isfile(f) and (not f.endswith('__init__.py') and not f.endswith('element.py'))
+]

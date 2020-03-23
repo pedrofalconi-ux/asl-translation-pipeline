8f0245dcebafa9307299e2c10ffc99bf571bb131
Samuel de Moura <samuel.moura@lavid.ufpb.br>
2020-03-23 13:23:22 -0300

    Implement filedest element

---
+++ src/elements/filedest.py
@@ -0,0 +1,40 @@
+import os
+
+from artifact import get_artifact_directory
+from elements.element import PipelineElement
+from registry import register_element
+
+class FileDestElement(PipelineElement):
+    '''Writes out to a file.'''
+    name = 'filedest'
+
+    _fd = None
+    _binary = False
+    _path = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, *kwargs)
+
+        try:
+            self._path = kwargs['path']
+            self._binary = getattr(kwargs, 'binary', False)
+            complete_path = os.path.join(get_artifact_directory(), self._path)
+            os.makedirs(os.path.dirname(complete_path), exist_ok=True)
+
+            self._fd = open(complete_path, 'wb' if self._binary else 'w')
+        except KeyError:
+            raise ValueError('`filedest` requires a `path` parameter.')
+
+    def process(self, data):
+        if self._binary:
+            self._fd.write(data)
+        else:
+            for line in data:
+                self._fd.write(line)
+
+    def __del__(self):
+        if self._fd:
+            self._fd.close()
+
+# Add element to the registry.
+register_element(FileDestElement)

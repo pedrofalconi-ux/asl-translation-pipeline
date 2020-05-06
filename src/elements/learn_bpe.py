e064f3eaa19e3383dcc34df4ea05024193b5a1c7
Samuel de Moura <samuel.moura@lavid.ufpb.br>
2020-05-06 14:25:46 -0300

    Clean up BPE elements

--- src/elements/learn_bpe.py
+++ src/elements/learn_bpe.py
@@ -1,31 +1,30 @@
+import logging
 import os
+
+from artifact import get_artifact_directory
 from elements.element import PipelineElement
 from registry import register_element
-from artifact import get_artifact_directory
-import logging
-
 
 class LearnBpeElement(PipelineElement):
-    """ Learn BPE Step
+    ''' Learn BPE Step
         Concatena gr e gi preprocessados e executa o learn bpe do subword-nmt na saída.
-    """
-    name = "LearnBPE"
-
+    '''
+    name = 'learn_bpe'
 
     def __init__(self, *args, **kwargs):
-        """Kwargs:
+        '''Kwargs:
            -src {string}: Source file extension ('gr' or 'gi').
            -tgt {string}: Target file extension ('gr' or 'gi').
            -bpe_tokens {int}: Number of merges(BPE).
-        """
+        '''
         super().__init__(args, *kwargs)
-        
+
         # set src to src argument if it's given, else set to default(gr)
         if 'src' in kwargs:
             self._src = kwargs['src']
         else:
             self._src = 'gr'
-            
+
         # set tgt to tgt argument if it's given, else set to default(gi)
         if 'tgt' in kwargs:
             self._tgt = kwargs['tgt']
@@ -36,32 +35,32 @@ class LearnBpeElement(PipelineElement):
         self._bpe_tokens = kwargs['bpe_tokens']
 
         # set BPE folder
-        self._bpe_path = os.path.join(get_artifact_directory(), "BPE")
+        self._bpe_path = os.path.join(get_artifact_directory(), 'BPE')
 
         # set preprocessed path
-        self._preprocessed_path = os.path.join(get_artifact_directory(), "Preprocessed")
+        self._preprocessed_path = os.path.join(get_artifact_directory(), 'Preprocessed')
 
         # set train concat corpus
-        self._train_concat_corpus = os.path.join(self._bpe_path, f"train.{self._src}-{self._tgt}")
+        self._train_concat_corpus = os.path.join(self._bpe_path, f'train.{self._src}-{self._tgt}')
         os.makedirs(os.path.dirname(self._train_concat_corpus), exist_ok=True)
 
         # set bpe_code
-        self._bpe_code = os.path.join(self._bpe_path, "bpe_code")
-        
+        self._bpe_code = os.path.join(self._bpe_path, 'bpe_code')
+
 
     def process(self, data=None):
         logger = logging.getLogger(__name__)
-        
-        #set path to files
-        preprocessed_train_gr_file = os.path.join(self._preprocessed_path, "train." + self._src)
-        preprocessed_train_gi_file = os.path.join(self._preprocessed_path, "train." + self._tgt)
 
-        #concatenation of preprocessed gr and gi to a single train file
-        os.system(f"cat {preprocessed_train_gr_file} {preprocessed_train_gi_file} > {self._train_concat_corpus}")
+        # set path to files
+        preprocessed_train_gr_file = os.path.join(self._preprocessed_path, 'train.' + self._src)
+        preprocessed_train_gi_file = os.path.join(self._preprocessed_path, 'train.' + self._tgt)
 
-        #run learn bpe using the concatenated file, with 'bpe_tokens'(number) merge operations
+        # concatenation of preprocessed gr and gi to a single train file
+        os.system(f'cat {preprocessed_train_gr_file} {preprocessed_train_gi_file} > {self._train_concat_corpus}')
+
+        # run learn bpe using the concatenated file, with 'bpe_tokens'(number) merge operations
         logger.debug(f'Running learn_bpe.py on {self._train_concat_corpus}')
-        os.system(f"subword-nmt learn-bpe -s {self._bpe_tokens} < {self._train_concat_corpus} > {self._bpe_code}")
+        os.system(f'subword-nmt learn-bpe -s {self._bpe_tokens} < {self._train_concat_corpus} > {self._bpe_code}')
 
 # Add element to the registry.
 register_element(LearnBpeElement)

f8195b1d41c4f371601c51359c68e7ef8556c0ce
Samuel de Moura <samuel.moura@lavid.ufpb.br>
2020-05-06 13:48:20 -0300

    Minor refactoring of add_submodule_to_sys_path

--- src/utils.py
+++ src/utils.py
@@ -16,8 +16,12 @@ def get_git_revision_hash(cwd=None, short=False):
 
     return subprocess.check_output(popenargs, cwd=cwd).decode('utf-8').strip()
 
+def get_submodule_path(submodule_name):
+    '''Gets the path to the submodule named `submodule_name`.'''
+    return os.path.realpath(
+        os.path.join(os.path.dirname(__file__), '..', 'submodules', submodule_name)
+    )
+
 def add_submodule_to_sys_path(submodule_name):
     '''Adds the given `submodule_name` to the sys's import PATH.'''
-    sys.path.insert(0, os.path.realpath(
-        os.path.join(os.path.dirname(__file__), '..', 'submodules', submodule_name))
-    )
+    sys.path.insert(0, get_submodule_path(submodule_name))

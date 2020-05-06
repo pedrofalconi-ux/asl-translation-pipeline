a13edbba425fa1d0bc0176c5814efbe4234ebe6b
Samuel de Moura <samuel.moura@lavid.ufpb.br>
2020-05-06 14:02:52 -0300

    Clean up and rename csvdest to parallel_filedest

--- src/elements/parallel_filedest.py
+++ src/elements/parallel_filedest.py
@@ -4,9 +4,9 @@ from artifact import get_artifact_directory
 from elements.element import PipelineElement
 from registry import register_element
 
-class CsvDestElement(PipelineElement):
-    '''Writes out to gr and gi.'''
-    name = 'csvdest'
+class ParallelFileDestElement(PipelineElement):
+    '''Writes out GR and GI files.'''
+    name = 'parallel_filedest'
 
     _gr_fd = None
     _gi_fd = None
@@ -21,14 +21,15 @@ class CsvDestElement(PipelineElement):
         try:
             self._gr_path = kwargs['gr_path']
             self._gi_path = kwargs['gi_path']
-            
+
             self._complete_gr_path = os.path.join(get_artifact_directory(), self._gr_path)
             os.makedirs(os.path.dirname(self._complete_gr_path), exist_ok=True)
             self._complete_gi_path = os.path.join(get_artifact_directory(), self._gi_path)
             os.makedirs(os.path.dirname(self._complete_gi_path), exist_ok=True)
-           
+
         except KeyError:
             raise ValueError('`csvdest` requires a `gr_path` and `gi_path` parameter.')
+
 
     def process(self, data):
         with open(self._complete_gr_path, 'w') as self._gr_fd, open(self._complete_gi_path, 'w') as self._gi_fd:
@@ -36,8 +37,6 @@ class CsvDestElement(PipelineElement):
                 self._gr_fd.write(f'{line[0]}\n')
                 self._gi_fd.write(f'{line[1]}\n')
 
-    def __del__(self):
-        pass
 
 # Add element to the registry.
-register_element(CsvDestElement)
+register_element(ParallelFileDestElement)

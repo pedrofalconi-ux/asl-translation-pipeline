a86a158c95157a6801c5151d720932eb16320a8a
renan-goes <36930305+Renan-Goes@users.noreply.github.com>
2020-11-12 12:55:29 -0300

    fixed augmentation places cutting of sentence

--- translation_pipeline/src/elements/augmentation_places.py
+++ translation_pipeline/src/elements/augmentation_places.py
@@ -62,9 +62,10 @@ class PlacesAugmentation(PipelineElement):
                 aug_data.append(aug_phrase)
 
         random.shuffle(aug_data)
+        pattern = rf'[A-ZÁÉÍÓÚÀÂÊÔÃÕÜÇa-záéíóúàâêôãõüç_]+(?=&ESTADO|&PAÍS|&CIDADE)'
+        
         data = data + list(aug_data)    # concatenates the original data with the augmented data (turned into a list)
         return data          
-
 
     def generate(self, data, places, max_new_sentences, max_num_places):
         augmented_phrases = set()
@@ -72,6 +73,7 @@ class PlacesAugmentation(PipelineElement):
 
         # Searches for places in the sentence, storing the names in one list, the types in another and the gi pattern in another
         gr_places_found, gi_places_found, gi_pattern = self.row_search(gr,gi,places)
+        
         gr_places_found = gr_places_found[:max_num_places]
         gi_places_found = gi_places_found[:max_num_places]
         gi_pattern = gi_pattern[:max_num_places]
@@ -81,33 +83,52 @@ class PlacesAugmentation(PipelineElement):
 
         # Creates all combinations of the places from the 'lugares.csv' file with their respective categories
         
-        #print(gi_places_found)
-        founded_places = [places[i] for i in gi_places_found]
-        s = product(*founded_places)
+        found_places = [places[i] for i in gi_places_found]
+        s = product(*found_places)
         possible_places = list(s)   # makes the combinations into a list
         random.shuffle(possible_places) # shuffles the combinations in order to not always create the same structures
         possible_places = [k for k in possible_places if len(k) == len(set(k))][:max_new_sentences] # removes repeated cases (e.g ('BRASIL', 'BRASIL')) and only takes the specified amount of combinations
 
-        if len(possible_places[0]) >= 1: # will only do the augmentation in case the element is not empty, i.e, there actually are places found in the sentence
+        if len(possible_places[0]) >= 1 and len(splited_gr) == len(splited_gi): # will only do the augmentation in case the element is not empty, i.e, there actually are places found in the sentence
             for places_comb in possible_places:    # goes through every possible combination, which will be substitute the original places in the sentence
                 new_phrase_gr = [] # The new gr sentence generated
                 new_phrase_gi = [] # The new gi sentence generated
 
+                count = 0   # counts the number of times it loops through the partitioned sentence
+
                 for pattern_gr, pattern_gi, item, part_gr, part_gi in zip(gr_places_found, gi_places_found, places_comb, splited_gr, splited_gi):
                     # each place in the sentence will be replaced by one of the combinations
-
-                    ex = re.search(fr'([{valid_chars}_])+(?=&PAÍS|&ESTADO|&CIDADE)',item).group(0) # searches for all the places found and puts the in a list (e.g "EU AMAR BRASIL&PAÍS" returns just "BRASIL")
-                    new_phrase_gr.append(re.sub(pattern_gr, ex, part_gr)) # makes the replacement of the nth place in the sentence in the nth partitioned sentence for the 'gr'
-
-                    new_phrase_gi.append(re.sub(pattern_gr+'&'+pattern_gi, item, part_gi)) # makes the replacement of the nth place in the sentence in the nth partitioned sentence for the 'gi'
+                    pattern_gr = r'\b' + pattern_gr + r'\b'
+                    pattern_gi = r'\b' + pattern_gi + r'\b'
                     
+                    ex = re.search(fr'([{valid_chars}_])+(?=&PAÍS|&ESTADO|&CIDADE)',item).group(0) # searches for all the places found and puts the in a list (e.g "EU AMAR BRASIL&PAÍS" returns just "BRASIL")
+                    
+                    new_phrase_gr.append(re.sub(pattern_gr, ex, part_gr)) # makes the replacement of the nth place in the sentence in the nth partitioned sentence for the 'gr'
+                    new_phrase_gi.append(re.sub(pattern_gr+'&'+pattern_gi, item, part_gi)) # makes the replacement of the nth place in the sentence in the nth partitioned sentence for the 'gi'
+
+                    count += 1
+
+                # inserts the end of the original sentence (without augmentable place) in the augmented sentence in case it didn't loop through all the partitions
+                if count < len(splited_gr):
+                    new_phrase_gr.append(splited_gr[-1])
+                    new_phrase_gi.append(splited_gi[-1])
+
                 augmented_phrases.add((''.join(new_phrase_gr), ''.join(new_phrase_gi)))   # concatenates the split augmented sentences into one sentence, like the original, but with the correct replacements
-            
+
         try:    # if the original sentence is found in the set of augmented sentences, then it's removed
             augmented_phrases.remove((gr, gi))
             return augmented_phrases
         except: # otherwise the function will just return the augmented sentences
             return augmented_phrases
+
+    def correspondence_aval(self, sentences, pattern):
+        for gr, gi in sentences:
+
+            places_gi = [word.replace('_', ' ') for word in re.findall(pattern, gi)]
+            places_gr = list(filter(lambda word: word in gr, places_gi))
+
+            if not places_gr == places_gi:
+                print(f'\nGR: {gr}\nplaces found in GR: {places_gr}\n\nGI: {gi}\nplaces found in GI: {places_gi}')
 
     def row_search(self, gr,gi,places):
         """Searches the sentence for the places that are given in the `lugares.csv` file
@@ -131,14 +152,16 @@ class PlacesAugmentation(PipelineElement):
 
         for place in re.findall(pattern,gi):  # for each place found the place itself, the type of place and the gi pattern will be stored for it
             place_split = place[0].split('&')
-            if place_split[0]+'&'+place_split[1] in places['PAÍS'] or place_split[0]+'&'+place_split[1] in places['ESTADO'] or place_split[0]+'&'+place_split[1] in places['CIDADE']:
+            if place_split[0]+'&'+place_split[1] in places['PAÍS'] \
+                or place_split[0]+'&'+place_split[1] in places['ESTADO'] \
+                or place_split[0]+'&'+place_split[1] in places['CIDADE']:
                 gr_places_found.append(place_split[0])
                 gi_places_found.append(place_split[1])
                 gi_pattern.append(place_split[0]+'&'+place_split[1])
 
         return gr_places_found, gi_places_found, gi_pattern
 
-    def split_phrase(self, phrase,pattern):
+    def split_phrase(self, phrase,patterns):
         """splits phrases by place, divinding one sentence into multiple sentences with one place in each.
         If the sentence has only one place, it will return a list with only the original sentence.
 
@@ -150,13 +173,15 @@ class PlacesAugmentation(PipelineElement):
             list of strings: Returns the list with each each part of the sentence that has a place in it
         """
         phrases = list()
-
-        for i in pattern:
-            new_phrase = phrase.split(i, 1)
-            try:
+        
+        for index, pattern in enumerate(patterns):
+            new_phrase = phrase.split(pattern, 1)
+            try:    # if the split was made, then concatenates the first splitted sentence into the list
                 phrase = new_phrase[1]
-                phrases.append(new_phrase[0] + i)
-            except:
+                phrases.append(new_phrase[0] + pattern)
+                if index == len(patterns) - 1:  # concatenates the end of the sentence, which doesn't include one of the places to augment
+                    phrases.append(new_phrase[1])
+            except: # if the split wasn't made (at the end of the sentence), then it inserts the end the sentence into the list
                 phrase = new_phrase[0]
                 phrases.append(new_phrase[0])
 

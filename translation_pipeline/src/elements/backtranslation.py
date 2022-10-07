4753fa06374b08c441a642c4729c7f31c777679d
diego.silva <diego.silva@lavid.ufpb.br>
2022-10-07 09:49:38 -0300

    Add new params to back translation

--- translation_pipeline/src/elements/backtranslation.py
+++ translation_pipeline/src/elements/backtranslation.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import random
 
 import torch
@@ -12,12 +11,10 @@ logger = logging.getLogger(__name__)
 
 
 class BackTranslationElement(PipelineElement):
-
     name = "back_translation"
-    version = 1
+    version = 2
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
         en_model_name = "Helsinki-NLP/opus-mt-ROMANCE-en"
         self.en_tokenizer = AutoTokenizer.from_pretrained(en_model_name)
@@ -31,20 +28,21 @@ class BackTranslationElement(PipelineElement):
         else:
             self._lang = kwargs["lang"]
 
-        self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self._ratio = float(kwargs["ratio"]) if "ratio" in kwargs else None
+        self._min_tokens = int(kwargs["min_tokens"]) if "min_tokens" in kwargs else None
 
         # Import TQDM.
         from tqdm import tqdm
 
         self._tqdm = tqdm
 
-    def translate(self, texts, model, tokenizer, task, language=None):
+    def translate(self, texts, model, tokenizer, language=None):
         # Tokenize the texts
         gen_pipeline = pipeline(
             "text2text-generation",
             model=model,
             tokenizer=tokenizer,
-            device=self._device,
+            device=0 if torch.cuda.is_available() else "cpu",
         )
 
         template = (
@@ -63,7 +61,6 @@ class BackTranslationElement(PipelineElement):
             texts,
             self.en_model,
             self.en_tokenizer,
-            task="translate Portuguese to English: ",
             language=self._lang,
         )
         en_texts = [text["generated_text"] for text in en_texts]
@@ -72,14 +69,28 @@ class BackTranslationElement(PipelineElement):
             en_texts,
             self.pt_model,
             self.pt_tokenizer,
-            task="translate English to Portuguese: ",
             language="pt_BR",
         )
         return [text["generated_text"] for text in back_translated_texts]
 
     def process(self, data):
         augmented_phrases = []
-        for pt, gi in self._tqdm(data, desc="back_translating"):
+
+        sampled_data = data.copy()
+
+        # Do not apply back translation when sentence has less than `min_tokens` tokens
+        if self._min_tokens:
+            sampled_data = [
+                t for t in sampled_data if len(t[0].split(" ")) > self._min_tokens
+            ]
+
+        # Only apply back translation on a subset of the data
+        if self._ratio:
+            random.shuffle(sampled_data)
+            index = round(len(data) * self._ratio)
+            sampled_data = sampled_data[:index]
+
+        for pt, gi in self._tqdm(sampled_data, desc="back_translating"):
             if not pt and not gi:
                 continue
             try:
@@ -89,6 +100,13 @@ class BackTranslationElement(PipelineElement):
             # Do not update if the translated sentence is equal to the original
             if new_pt != pt:
                 augmented_phrases.append((new_pt, gi))
+
+        # Log a few random samples from the augmented_phrases
+        logger.info(f"Generated {len(augmented_phrases)} new augmented phrases")
+        for index in random.sample(range(len(augmented_phrases)), 3):
+            logger.info(
+                f"Sample {index} of the augmented phrases: {augmented_phrases[index]}."
+            )
 
         random.shuffle(augmented_phrases)
         data.extend(augmented_phrases)

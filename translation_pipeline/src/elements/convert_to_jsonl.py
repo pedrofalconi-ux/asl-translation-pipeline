b05bdc2ede62d56c6fddd13c1cda1b92651ae8e3
diego.silva <diego.silva@lavid.ufpb.br>
2022-11-22 10:50:07 -0300

    feat: Add convert to jsonl (pipeline element)

---
+++ translation_pipeline/src/elements/convert_to_jsonl.py
@@ -0,0 +1,16 @@
+from elements.element import PipelineElement
+from registry import register_element
+
+class ConvertToJSONLElement(PipelineElement):
+    name = 'convert_to_jsonl'
+    dont_use_cache = True
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def process(self, data):
+        data_tuples = [{"translation": {'pt': row[0], 'gi': row[1]}} for row in data]
+        return data_tuples
+
+# Add element to the registry.
+register_element(ConvertToJSONLElement)

6d772e74adfc3808c7b3fa3407372992518d5c17
diego.silva <diego.silva@lavid.ufpb.br>
2022-11-22 10:59:05 -0300

    chore: chore: apply pre-commit (enforce code style)

--- translation_pipeline/src/elements/text_normalize.py
+++ translation_pipeline/src/elements/text_normalize.py
@@ -1,12 +1,12 @@
 from elements.element import PipelineElement
 from registry import register_element
-from utils import get_file_md5_hash
 from tqdm.auto import tqdm
+from utils import get_file_md5_hash
+from vlibras_preprocessing.preprocessor import TextProcessor
 
-from vlibras_preprocessing.preprocessor import TextProcessor 
 
 class TextNormalizeElement(PipelineElement):
-    name = 'text_normalize'
+    name = "text_normalize"
     version = 1
 
     def __init__(self, *args, **kwargs):
@@ -17,10 +17,11 @@ class TextNormalizeElement(PipelineElement):
     def process(self, data):
         transformed_data = []
 
-        for pt, gi in tqdm(data): 
+        for pt, gi in tqdm(data):
             new_pt = self._processor.process(pt)
             transformed_data.append((new_pt, gi))
         return transformed_data
-   
+
+
 # Add element to the registry.
 register_element(TextNormalizeElement)
